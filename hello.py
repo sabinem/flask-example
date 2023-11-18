5bb2ad8cf3060415d7adf35d47b0552216d99312
{sabine.maennel} <{sabine.maennel@gmail.com}>
2023-11-18 20:36:45 +0100

    add one more paragraph

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World!</h1><p>Have a great day!</p>"
+    return "<h1>Hello, World!</h1><p>Have a great day!</p><p>See you soon</p>"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
